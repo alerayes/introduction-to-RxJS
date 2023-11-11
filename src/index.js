1aa1691ba6c65e95ddba8d1532bb0ea9ac2ad9a5
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-11 18:55:01 -0300

    Demonstrates fromEvent operator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { timer, interval } from "rxjs";
+import { timer, 
+        interval, 
+        fromEvent } from "rxjs";
 
-const intervalObserable = interval(1000)
+// const observable = interval(1000)
 
-const timerObservable =  timer(0,1000)
+// const observable =  timer(0,1000)
 
-
+const observable = fromEvent(
+    document, 'click'
+)
 
 const subscription = observable.subscribe(
     console.log

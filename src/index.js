5b6e2124b0820d75e57e716ec2a81c08aac4e9eb
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-12 19:09:02 -0300

    Demonstrates the switchMap operator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { fromEvent, interval } from "rxjs";
-import { map, mergeMap, take, tap } from 'rxjs/operators'
+import { map, switchMap, take, tap } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 
 const button = document.querySelector('#btn')
@@ -7,10 +7,14 @@ const button = document.querySelector('#btn')
 const observable = fromEvent(
     button, 'click'
 ).pipe(
-    mergeMap(() => {
-        return interval(1000).pipe(
-            tap(console.log),
-            take(5)
+    switchMap(() => {
+        return ajax.getJSON('https://jsonplaceholder.typicode.com/todos/1').pipe(
+            take(5),
+            tap({
+                complete(){
+                    console.log('inner observable has been completed')
+                }
+            }),
         )
     }),
 )
@@ -26,5 +30,15 @@ const subscription = observable.subscribe({
 
 console.log('hello')
 
-// The mergeMap operator will subscribe to an observable if
-// it's returned by the function passed into it.
+// It performs a similar task to the mergeMap operator. Inner 
+// observables are automatically subscribed by this operator.
+
+// The mergeMap operator will not limit the number of inner active
+// observables. On the other hand, the switchMap operator will
+// limit the number of active, inner observables to one.
+
+// It is considered one of the safest flatenning operators.
+
+// It is great for managing one active subscription at a time.
+
+// One realistic scenario for this operator is to handle requests.
